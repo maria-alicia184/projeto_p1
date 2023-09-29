fc02cfa89b9900790c03f6efa6870fa2be3182a2
Maria Alicia <maria.alicia184@outlook.com>
2023-09-29 01:20:54 -0300

    P1(Projeto Jogo de Dados): melhorias pt.3 (final)

--- bin/p1.dart
+++ bin/p1.dart
@@ -1,9 +1,8 @@
 import 'dart:io';
 import 'dart:math';
 
-//1. Dados comuns: 3 dados comuns, de 6 lados
+// 1. Dados comuns: 3 dados comuns, de 6 lados
 int dadosComuns(Random gerador) {
-  // Dados comuns
   var dado1 = gerador.nextInt(6) + 1;
   var dado2 = gerador.nextInt(6) + 1;
   var dado3 = gerador.nextInt(6) + 1;
@@ -13,36 +12,34 @@ int dadosComuns(Random gerador) {
   return somaDados;
 }
 
-//2. Dado magico
+// 2. Dado magico
 double dadoMagico(Random gerador) {
-  // Dado magico gera o valor 0 ou 1
+  // Dado mágico gera o valor 0 ou 1
   var dadoMagico = gerador.nextInt(2);
-  double somaDados = 0;
+  double multiplicador = 1.0; // Valor padrão do multiplicador
 
-  // Condicional do dado magico
+  // Condicional do dado mágico
   if (dadoMagico == 1) {
-    somaDados = somaDados * 2;
+    multiplicador = 2.0;
   } else if (dadoMagico == 0) {
-    somaDados = somaDados / 2;
+    multiplicador = 0.5;
   }
-  return somaDados;
+  return multiplicador;
 }
 
-//3. Jogo de dados
+// 3. Jogo de dados
 void main() {
   var gerador = Random();
   int opcao;
   String nomeUsuario;
+  int opcDadoMagico;
 
   // Exibir menu de opções
   stdout.writeln('Digite seu nome:');
   nomeUsuario = stdin.readLineSync()!;
+  stdout.writeln('Digite um numero do menu de opções:');
   stdout.writeln('Menu de opções\n0 - Sair\n1 a 50 - Numero de rodadas\n');
-  stdout.writeln(
-      'Limitamos o numero de rodadas em 50 para não ficar muito exaustivo\n');
-  stdout.writeln('Digite o numero de rodadas que deseja jogar:');
   opcao = int.tryParse(stdin.readLineSync()!) ?? 0;
-
   if (opcao == 0) {
     print('Saindo...');
     print('Tchau!');
@@ -51,15 +48,15 @@ void main() {
     print('Numero de rodadas invalido, valor deve estar entre 1 e 50');
     return;
   }
-  print('Numero de rodadas: $opcao');
 
   // Iniciar jogo com número de rodadas escolhido pelo usuário
+  print('Numero de rodadas: $opcao');
   int usuario = 0;
   int placarUsuario = 0;
   int computador = 0;
   int placarComputador = 0;
   int placarEmpate = 0;
-  //inicio da rodada usuario x computador
+  // Início da rodada usuario x computador
   for (int i = 0; i < opcao; i++) {
     print('\n------------------------------------------------------');
     print('|       \t\tRodada ${i + 1}\t\t     |');
@@ -83,12 +80,28 @@ void main() {
     print(
         '\nPlacar da Rodada:\n$nomeUsuario: $placarUsuario \tComputador: $placarComputador \tEmpates: $placarEmpate');
   }
+  //Ao fim das rodadas, perguntar se o usuário deseja usar o dado mágico
+  stdout.writeln('\nDeseja usar o dado mágico? \n0 - Não \n1 - Sim');
+  opcDadoMagico = int.tryParse(stdin.readLineSync()!) ?? 0;
+  if (opcDadoMagico == 0) {
+    print('\nVocê não usou o dado mágico!');
+  } else if (opcDadoMagico == 1) {
+    print('\nVocê usou o dado mágico!');
+    double multiplicador = dadoMagico(gerador);
+    int placarMultiplicado = (placarUsuario * multiplicador).toInt();
+    print('Placar usuário: $placarUsuario');
+    print('Multiplicador: $multiplicador');
+    placarUsuario = placarMultiplicado;
+  } else {
+    print('Opção inválida!');
+    return;
+  }
+
   // Exibir placar final
   print('\n\n*************************************************');
   print(
       '*  Placar final: \t\t\t\t*\n*  $nomeUsuario: $placarUsuario \tComputador: $placarComputador \tEmpates: $placarEmpate      *');
   print('*************************************************');
-
   // Exibir vencedor
   if (placarUsuario > placarComputador) {
     print('$nomeUsuario venceu!  ( • ᴗ • )');
